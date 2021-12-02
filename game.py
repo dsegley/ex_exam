9d0fe1f89bed5ec0f948f0e8d9821448cd864dc3
DavidSegoviaLey <dsegovia@frd.org.mx>
2021-12-02 10:29:27 -0600

    Añadido opcion de repaso y abrir urls en el navegador

--- game.py
+++ game.py
@@ -2,15 +2,18 @@
 import os
 import random
 import common.get_questions as get_questions
+import webbrowser
 from datetime import datetime
 
 questions = []
+wrong_ans = []
 
 def format(content):
     text = ""
     correct_ans = ""
     reference = ""
     explanation = ""
+    url = ""
     in_explanation = False
     first = True
     for line in content:
@@ -23,7 +26,8 @@ def format(content):
                 "text": text, 
                 "ans": correct_ans, 
                 "reference": reference,
-                "explanation": explanation
+                "explanation": explanation,
+                'url': url
             })
             text = line
             in_explanation = False
@@ -45,6 +49,7 @@ def format(content):
         elif 'https' in line:
             index = line.find('https')
             reference = "Reference: " + line[index:]
+            url = line[index:]
         
         first = False
 
@@ -57,10 +62,64 @@ def format(content):
     })
 
 
+def quiz(ques, num=0):
+    score = 0
+    good_ans = []
+
+    random.shuffle(ques)
+    if num == 0:
+        num = len(ques)
+
+    for i in range(0, num):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"{i + 1}/{num}")
+        print(ques[i]['text'])
+        
+        ans = ''
+        while len(ans) == 0:
+            try:
+                ans = input('\nRespuesta: ').upper()
+            except ValueError as err:
+                pass
+
+        corr_ans = ques[i]['ans']
+        expla = ques[i]['explanation']
+        ref = ques[i]['reference']
+        url = ques[i]['url']
+
+        if sorted(ans) == sorted(corr_ans):
+            score = score + 1
+            print('\n¡Correcto!')
+            if ques[i] in wrong_ans:
+                good_ans.append(i)
+        else:
+            print(f"\nIncorrecto\nRespuesta correcta: {corr_ans}")
+            if ques[i] not in wrong_ans:
+                wrong_ans.append(ques[i])
+
+        print()
+        if len(expla) > 0:
+            print(expla)
+
+        if len(ref) > 0:
+            print(ref)
+            print("Presione la tecla 'O' para abrir la url en el navegador")
+
+        open = input("Presione una tecla para continuar... ").lower()
+        if (open == 'o' and len(ref) > 0):
+                webbrowser.open(url)
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    for i in good_ans:
+        if ques[i] in wrong_ans:
+            wrong_ans.pop(i)
+
+    return score
+
+
 def game():
     num_of_exer = 0
-    score = 0
-
     print('GPI v.1.0')
 
     while num_of_exer == 0 or num_of_exer > len(questions):
@@ -70,42 +129,15 @@ def game():
         except ValueError as err:
             print('Ingrese un numero valido ')
 
-    os.system('cls' if os.name == 'nt' else 'clear')
-    
-    random.shuffle(questions)
-
-    for i in range(0, num_of_exer):
-        print(f"{i + 1}/{num_of_exer}")
-        print(questions[i]['text'])
-        
-        ans = ''
-        while len(ans) == 0:
-            try:
-                ans = input('\nRespuesta: ').upper()
-            except ValueError as err:
-                pass
-
-        corr_ans = questions[i]['ans']
-        expla = questions[i]['explanation']
-        ref = questions[i]['reference']
-
-        if sorted(ans) == sorted(corr_ans):
-            score = score + 1
-            print('\n¡Correcto!')
-        else:
-            print(f"\nIncorrecto\nRespuesta correcta: {corr_ans}")
-
-        print()
-        if len(expla) > 0:
-            print(expla)
-
-        if len(ref) > 0:
-            print(ref)
-
-        input()
-        os.system('cls' if os.name == 'nt' else 'clear')
-
+    score = quiz(questions, num_of_exer)
     input(f"\nAcertó {score}/{num_of_exer}\n{score/num_of_exer * 100}% ")
+
+    r = 'y'
+    while r == 'y' and len(wrong_ans) > 0:
+        r = input('¿Desea repasar las preguntas sin acertar?(y/n): ').lower()
+        if (r == 'y'):
+            quiz(wrong_ans)
+    wrong_ans.clear()
 
 
 if __name__ == '__main__':
